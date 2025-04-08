3383e2d970982801e3226fc95c3cfb057dea4a68
AdrianRp7 <parc769@gmail.com>
2025-04-08 17:26:43 +0200

    improve: this adds a loader when the description is loading

--- pages/series/[id].vue
+++ pages/series/[id].vue
@@ -1,5 +1,14 @@
 <template>
-    <MediaDescription :id="id" />
+    <Suspense>
+        <template #default>
+            <MediaDescription :id="id" />
+        </template>
+        <template #fallback>
+            <div class="grid place-items-center h-full">
+                <UtilsLoader :classes-spin="'fill-green-400 dark:fill-green-500 size-12 text-white mx-auto mb-8'"/>
+            </div>
+        </template>
+    </Suspense>
 </template>
 
 <script lang="ts" setup>
